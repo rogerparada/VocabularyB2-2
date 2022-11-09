391bf4cef233b42f3c7182d9d330d57d4056b325
Roger Parada <60521155+rogerparada@users.noreply.github.com>
2022-11-09 16:55:54 +0100

    Update list and add unit 3

--- src/components/ListComponent.jsx
+++ src/components/ListComponent.jsx
@@ -1,17 +1,33 @@
 import React from "react";
-import { getWords } from "../services/WordsService";
+import { getUnitWords } from "../services/WordsService";
 import ListItemComponent from "./ListItemComponent";
 import DropdownComponent from "./DropdownComponent";
 
 export default function ListComponent() {
-	const words = getWords();
+	const unit1 = getUnitWords(1);
+	const unit3 = getUnitWords(3);
 
 	return (
-		<div className="flex flex-wrap justify-center gap-5">
+		<div className="mx-3 mb-10">
 			<DropdownComponent />
-			{words.map((word, index) => {
-				return <ListItemComponent word={word} key={index} />;
-			})}
+			<div className="flex flex-row mt-5">
+				<h1 className=" font-bold">Unit 1 & 2</h1>
+			</div>
+			<hr className="mb-5" />
+			<div className="flex flex-wrap justify-center gap-5">
+				{unit1.map((word, index) => {
+					return <ListItemComponent word={word.word} key={index} />;
+				})}
+			</div>
+			<div className="flex flex-row mt-5">
+				<h1 className=" font-bold">Unit 3</h1>
+			</div>
+			<hr className="mb-5" />
+			<div className="flex flex-wrap justify-around gap-5">
+				{unit3.map((word, index) => {
+					return <ListItemComponent word={word.word} key={index} />;
+				})}
+			</div>
 		</div>
 	);
 }
--- src/services/WordsService.js
+++ src/services/WordsService.js
@@ -1,38 +1,58 @@
 const words = [
-	"inhabitants",
-	"heir",
-	"remain",
-	"hope",
-	"frightening",
-	"afraid",
-	"awkward",
-	"concerned",
-	"produce",
-	"setup",
-	"unprivileged",
-	"contagion",
-	"either",
-	"stubborn",
-	"wether",
-	"toughest",
-	"selfish",
-	"genuine",
-	"spot",
-	"spooky",
-	"halves",
-	"swollen",
-	"blister",
-	"ankle",	
-	"bleed",
-	"aid",
-	"stretcher",
-	"leaning",
-	"scruffy",
-	"seizure"	
+	{ word: "inhabitants", u: 1 },
+	{ word: "heir", u: 1 },
+	{ word: "remain", u: 1 },
+	{ word: "hope", u: 1 },
+	{ word: "frightening", u: 1 },
+	{ word: "afraid", u: 1 },
+	{ word: "awkward", u: 1 },
+	{ word: "concerned", u: 1 },
+	{ word: "produce", u: 1 },
+	{ word: "setup", u: 1 },
+	{ word: "unprivileged", u: 1 },
+	{ word: "contagion", u: 1 },
+	{ word: "either", u: 1 },
+	{ word: "stubborn", u: 1 },
+	{ word: "wether", u: 1 },
+	{ word: "toughest", u: 1 },
+	{ word: "selfish", u: 1 },
+	{ word: "genuine", u: 1 },
+	{ word: "spot", u: 1 },
+	{ word: "spooky", u: 1 },
+	{ word: "halves", u: 1 },
+	{ word: "swollen", u: 1 },
+	{ word: "blister", u: 1 },
+	{ word: "ankle", u: 1 },
+	{ word: "bleed", u: 1 },
+	{ word: "aid", u: 1 },
+	{ word: "stretcher", u: 1 },
+	{ word: "leaning", u: 1 },
+	{ word: "scruffy", u: 1 },
+	{ word: "seizure", u: 1 },
+	{ word: "symmetrical", u: 1 },
+	{ word: "unreachable", u: 1 },
+	{ word: "harm", u: 1 },
+	{ word: "seldom", u: 1 },
+	{ word: "wish", u: 1 },
+	{ word: "entrepreneur", u: 1 },
+	{ word: "races", u: 1 },
+	{ word: "skydiver", u: 1 },
+	{ word: "bias", u: 1 },
+	{ word: "regardless", u: 1 },
+	{ word: "suede", u: 1 },
+	{ word: "deaf", u: 3 },
+	{ word: "night-shift", u: 3 },
+	{ word: "customs", u: 3 },
+	{ word: "aisle", u: 3 },
+	{ word: "bumped", u: 3 },
 ];
 
 function getWords() {
 	return words.sort();
+}
+
+function getUnitWords(unit = 1) {
+	return words.sort().filter((x) => x.u === unit);
 }
 
 function getLastWords(items) {
@@ -134,4 +154,4 @@ function processMeaning(meaningsArray) {
 	return meanings;
 }
 
-export { getWords, getLastWords, getDefinition };
+export { getWords, getUnitWords, getLastWords, getDefinition };
